409dcb7469b4a33ad426d351be334d0d6565afc3
andresyrg16 <andresyrg16@gmail.com>
2022-07-19 10:33:50 -0500

    four commit

--- api/src/models/Generos.js
+++ api/src/models/Generos.js
@@ -6,12 +6,16 @@ module.exports = (sequelize) => {
   sequelize.define('Genero', {
     id: {
       type: DataTypes.INTEGER,
-      allowNull : false,
+      allowNull: false,
       primaryKey: true
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     }
-  });
+  },
+    {
+      createdAt: false,
+      updatedAt: false
+    });
 };
