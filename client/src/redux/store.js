c9085c0808d04a072b00ec35de4963246a5f0a5c
andresyrg16 <andresyrg16@gmail.com>
2022-07-21 16:50:57 -0500

    caperta de componentes y redux

---
+++ client/src/redux/store.js
@@ -0,0 +1,18 @@
+import { applyMiddleware, compose } from "redux";
+import { createStore } from "redux";
+import rootReducer from "../reducers/index";
+import thunk from "redux-thunk";
+
+
+const composeEnhancers =
+	(typeof window !== 'undefined' &&
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+	compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+
+);
+
+export default store;
