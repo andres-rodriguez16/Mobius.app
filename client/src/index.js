7f5054b3f680839ccdc38a769891eedfb4c929f1
andresyrg16 <andresyrg16@gmail.com>
2022-07-22 10:19:51 -0500

    index final, conectando todo con redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
-import store from "./redux/store"
+//import store from "./redux/store"
+import { Router, BrowserRouter } from "react-router-dom"
+import store from "./redux/store/store"
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>,
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
