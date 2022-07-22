7d195a2e8d141f2dab424b55495fef326b65259b
andresyrg16 <andresyrg16@gmail.com>
2022-07-22 18:26:23 -0500

    agregando home, u navbar al ruteo

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route } from "react-router-dom"
 import LandingPage from './componets/landinPage/LandingPage';
+import Home from './componets/Home/Home';
+import NavBar from "./componets/Navbar/Navbar"
+
 
 function App() {
   return (
    <React.Fragment>
-    <Route exact path="/" component={LandingPage} />   
+    <NavBar />
+    <Route exact path="/" component={LandingPage} /> 
+    <Route  path="/home" component={Home} />
    </React.Fragment>
 
   );
