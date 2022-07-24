532704723abbddf36ec1298279f8bebf4f65be89
andresyrg16 <andresyrg16@gmail.com>
2022-07-24 11:05:46 -0500

    terminado landingPage, pero sin estilos

--- client/src/componets/landinPage/LandingPage.jsx
+++ client/src/componets/landinPage/LandingPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from "./LandingPage.module.css"
 
 export default function LandingPage() {
   return (
     <div>
       <h1>Bienvenidos a mi pagina</h1>
       <Link to ="/home"> 
-        <button>Ingresar</button>
+        <button className={styles.button__into}>ingresar</button>
       </Link>
     </div>
   );
