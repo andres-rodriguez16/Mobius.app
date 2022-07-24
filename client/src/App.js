3fc7c0e3cf53ccef7a5afa616ce67ac60d8f7854
andresyrg16 <andresyrg16@gmail.com>
2022-07-24 11:01:22 -0500

    ajustes en la rutas, del details

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom"
+import { Route } from "react-router-dom"
 import LandingPage from './componets/landinPage/LandingPage';
 import Home from './componets/Home/Home';
 import NavBar from "./componets/Navbar/Navbar"
-
+import Details from './componets/Details/Details';
 
 function App() {
   return (
    <React.Fragment>
-    <NavBar />
+    <NavBar />  
     <Route exact path="/" component={LandingPage} /> 
-    <Route  path="/home" component={Home} />
+    <Route exact path="/home" component={Home} />
+    <Route exact path="/home/:id" component={Details} /> 
    </React.Fragment>
-
   );
 }
 
