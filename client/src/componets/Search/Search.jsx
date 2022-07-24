5077f2aba33940d19b0dd23e616ecd10d2b59350
andresyrg16 <andresyrg16@gmail.com>
2022-07-24 11:04:08 -0500

    search casi terminado, pero hay acomodar para que se vea el resultado el paginado

---
+++ client/src/componets/Search/Search.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { getVideoGamePorSearch } from "../../redux/actions/action";
+
+const Search = () =>{
+ const [name, setName] = useState("");
+ const dispacth = useDispatch();
+
+ function handleInputChange(e){
+   e.preventDefault();
+   setName(e.target.value)
+   console.log(name)
+ }
+function handleSubmit(e){
+  e.preventDefault();
+  dispacth(getVideoGamePorSearch(name))
+}
+
+  return (
+     <div>
+       <h2>Buscador</h2>
+       <div>
+            <label className="label" htmlFor="title">
+            
+            </label>
+            <input
+              type="text"
+              id="title"
+              autoComplete="off"
+              placeholder="buscar..."
+              onChange={(e) => handleInputChange(e)}
+            />
+             <button type="submit" onClick={(e) => handleSubmit(e)}>BUSCAR</button>
+          </div>
+         
+      </div>
+  )
+}
+
+export default Search;
