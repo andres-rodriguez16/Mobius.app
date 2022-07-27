425f22789c6b9f4e731a3395cdeac8592322d3c6
andresyrg16 <andresyrg16@gmail.com>
2022-07-27 09:51:24 -0500

    agrego la funcinalidad a los flitros

--- client/src/componets/Home/Home.jsx
+++ client/src/componets/Home/Home.jsx
@@ -1,42 +1,90 @@
 import React from 'react';
 import { useEffect , useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getVideoGames } from '../../redux/actions/action';
+import { getVideoGames, getGenres, ordenPorGames, filtroPorGenero , ratingSort, filtroPorAlpha} from '../../redux/actions/action';
 import Card from '../Card/Card';
 import { Link } from 'react-router-dom';
 import spinner from '../../media/spinner.svg';
 import Paginado from '../Paginado/Paginado';
 import Search from '../Search/Search';
+
+
+
 const Home = () => {
   const dispatch = useDispatch();
-  const videoGames = useSelector(state => state.videoGames);
-
+  const videoGames = useSelector(state => state.videoGamesFilter);
+  const genres = useSelector(state => state.genres)
   const [actualPage, setActualPage] = useState(1);
-  const [personajesPorPagina, setPersonajesPorPagina] = useState(15);
-  const indeceDelUltimoPersonaje = actualPage * personajesPorPagina; //? 1 * 15 = 15
-  const indeceDelPrimerPersonaje = indeceDelUltimoPersonaje - personajesPorPagina; // ? 15 - 15 = 0  
-  const actualesPersonajes = videoGames.slice(indeceDelPrimerPersonaje, indeceDelUltimoPersonaje); // ? 
+  const [videogamesPorPagina, setvideogamesPorPagina] = useState(15);
+  const indeceDelUltimoVideogame = actualPage * videogamesPorPagina; //? 1 * 15 = 15
+  const indeceDelPrimerVideogame = indeceDelUltimoVideogame - videogamesPorPagina; // ? 15 - 15 = 0  
+  const actualesVideogames = videoGames.slice(indeceDelPrimerVideogame, indeceDelUltimoVideogame); // ? 
   
 
   const paginado = (numeroDePaginada) => {
-     setActualPage(numeroDePaginada)
+    setActualPage(numeroDePaginada)
   }
 
   useEffect(() => {
     dispatch(getVideoGames());
+   dispatch(getGenres())
   }, [dispatch]);
 
+  // useEffect(()=>{
+  //   dispatch(getGenres())
+  // }, [])  
+
+function handleOnGames (e){
+   dispatch(ordenPorGames(e.target.value))
+}
+
+function handleFiltroPorGenro (e){
+  dispatch(filtroPorGenero(e.target.value))
+}
+
+function handleFiltroPorRating(e) {
+  dispatch(ratingSort(e.target.value))
+}
+function handleFiltroPorAlpha (e){
+  dispatch(filtroPorAlpha(e.target.value))
+}
   return (
     <div>
+
       <h1>Home</h1>
+      <div>
+        <select onChange={(e) => handleFiltroPorRating(e)}>
+          <option>Rating</option>
+          <option value='asc'>Ascendente</option>
+          <option value='desc'>Decendente</option>
+        </select>
+        <select onChange={(e) => handleFiltroPorAlpha(e)}>
+          <option value="ordenAlpha">Alpha</option>
+          <option value='a-z'>A-Z</option>
+          <option value='z-a'>Z-A</option>
+        </select>
+        <select onChange={(e) => handleOnGames(e)}>
+          <option>orden</option>
+          <option value="DB">Creados</option>
+          <option value="Api">Api</option>
+        </select>
+        <select name='Genros' onChange={(e) => handleFiltroPorGenro(e)}>
+          <option value="All">Todos</option>
+        {genres.length ? genres.map(g => {
+           return (
+             <option value={g.name} key={g.id} >{g.name}</option>
+           )
+        }): []} 
+        </select>
+      </div>
       <Paginado 
        videgames={videoGames.length}
-       personajesPorPagina={personajesPorPagina}
+       videogamesPorPagina={videogamesPorPagina}
        paginado={paginado}
       />
-      <Search/>
-      {actualesPersonajes.length ? (
-        actualesPersonajes?.map(v => {
+      <Search setActualPage={setActualPage} />
+      {actualesVideogames.length ? (
+        actualesVideogames?.map(v => {
           return (
             <div className='card' key={v.id}>
               <Link to={'/home/' + v.id} key={v.id}>
@@ -45,6 +93,7 @@ const Home = () => {
                   genres={v.genres}
                   image={v.img}
                   key={v.id}
+                  rating={v.rating}
                 />
               </Link>
             </div>
