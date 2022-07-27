785fdd7ebd1996e6036ef35060ec1687f065ee1a
andresyrg16 <andresyrg16@gmail.com>
2022-07-27 09:46:06 -0500

    elimino el Route

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 //import store from "./redux/store"
-import { Router, BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import store from "./redux/store/store"
 
 
