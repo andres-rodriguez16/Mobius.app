abcde1e32da0f4e4ac04f05c62f8ed2478ba6040
andresyrg16 <andresyrg16@gmail.com>
2022-07-27 09:38:58 -0500

    ya no se agregan los generos al momento de solicitar a la api

--- api/src/routes/genres.js
+++ api/src/routes/genres.js
@@ -1,24 +1,18 @@
 require('dotenv').config();
 const { Router } = require("express")
-const { YOUR_API_KEY } = process.env;
-const axios = require("axios")
+
 const { Genero } = require("../db")
 
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const { data } = await axios.get(`https://api.rawg.io/api/genres?key=${YOUR_API_KEY}`)
-  const { results } = data;
-  const genresName = results.map(g => g.name);
-  const genresPending = genresName.map(async(genre) => {
-    await Genero.create({ name: genre });
-  });
-  Promise.all(genresPending)
-  .then(async () => { 
-    const genresCreateds = await Genero.findAll();
-    res.json(genresCreateds);
-  })
-  .catch( (error) => {throw(error)})
+  try {
+    let encotrarGeneros = await Genero.findAll()
+    console.log(encotrarGeneros)
+   res.send(encotrarGeneros)
+  } catch (error) {
+    res.json({error : error})
+  }
 });
 
 module.exports = router;
