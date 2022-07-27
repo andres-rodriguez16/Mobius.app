8dd0f4eba31292c1b4ce12a0333e914ed1317436
andresyrg16 <andresyrg16@gmail.com>
2022-07-27 09:53:29 -0500

    elimino el formato de los filtros

--- client/src/componets/Navbar/Navbar.jsx
+++ client/src/componets/Navbar/Navbar.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { getVideoGames } from '../../redux/actions/action';
-import Search from '../Search/Search';
+import { getVideoGames} from '../../redux/actions/action';
+
 
 const NavBar = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getVideoGames());
-  }, [dispatch]);
-
-  function handleClick(e) {
-    e.preventDefault();
-    dispatch(getVideoGames());
-  }
-
+  }, [dispatch]); 
+   
   return (
     <header className='navBar'>
       <h1>VideoGames</h1>
@@ -28,16 +23,6 @@ const NavBar = () => {
           </li>
         </ul>
       </nav>
-      <div>
-        <select>
-          <option value='asc'>asc</option>
-          <option value='desc'>desc</option>
-        </select>
-        <select>
-          <option value='a-z'>a-z</option>
-          <option value='z-a'>z-a</option>
-        </select>
-      </div>
     </header>
   );
 };
