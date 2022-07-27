84e9d05a410d56422fcb1d0fd048bdf6640caaba
andresyrg16 <andresyrg16@gmail.com>
2022-07-27 09:55:29 -0500

    mofifico el nombre de las propiedades y del los botones del paginado

--- client/src/componets/Paginado/Paginado.jsx
+++ client/src/componets/Paginado/Paginado.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 
-const Paginado = ({ videgames, personajesPorPagina, paginado }) => {
+const Paginado = ({ videgames, videogamesPorPagina, paginado }) => {
   const numerosDePaginas = [];
-  for (let i = 1; i <= Math.ceil(videgames/personajesPorPagina); i++) {
-     numerosDePaginas.push(i)
+  for (let i = 1; i <= Math.ceil(videgames / videogamesPorPagina); i++) {
+    numerosDePaginas.push(i);
   }
-   
   return (
-     <nav>
-       <ul className='paginado'>
-        {
-          numerosDePaginas && numerosDePaginas.map(p => (
-            <li className='Number' key={p}>
-              <a onClick={() => paginado(p)}>{p}</a>
-            </li> 
+    <nav>
+      <ul className='paginado'>
+        {numerosDePaginas &&
+          numerosDePaginas.map(p => (
+            <button className='Number' key={p} onClick={() => paginado(p)}>
+              {p}
+            </button>
           ))}
-       </ul>
-     </nav>
-  )
+      </ul>
+    </nav>
+  );
 };
 
 export default Paginado;
