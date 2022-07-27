041b7435c81aa4e8590e491a673c01b8d54466d6
andresyrg16 <andresyrg16@gmail.com>
2022-07-27 09:59:31 -0500

    acciones de los filtros

--- client/src/redux/actions/action.js
+++ client/src/redux/actions/action.js
@@ -1,7 +1,12 @@
 
 export const GET_VIDEOGAMES = "GET_VIDEOGAMES";
 export const GET_VIDEOGAMES_POR_ID = "GET_VIDEOGAMES_POR_ID";
-export const GET_VIDEOGAME_POR_SEARCH =  "GET_VIDEOGAME_POR_SEARCH";
+export const GET_VIDEOGAME_POR_SEARCH = "GET_VIDEOGAME_POR_SEARCH";
+export const GET_GENRES = "GET_GENRES";
+export const RATING_SORT = "RATING_SORT";
+export const GAMES_SORT= "GAMES_SORT";
+export const TYPE_GENRO = "TYPE_GENRO";
+export const ORDEN_BY_ALPHA = "ORDEN_BY_ALPHA";
 
 export function getVideoGames() {
   return function (dispatch) {
@@ -13,7 +18,7 @@ export function getVideoGames() {
           payload: v
         });
       })
-      .catch(e => e)
+      .catch(e => console.log(e))
   };
 };
 
@@ -27,7 +32,7 @@ export function getVideoGamePorId(id) {
           payload: response
         })
       })
-      .catch(e => e)
+      .catch(e => console.log(e))
   }
 }
 
@@ -41,6 +46,49 @@ export function getVideoGamePorSearch(name) {
           payload: response,
         })
       })
-      .catch(e => e)
+      .catch(e => console.log(e))
+  }
+}
+
+export function getGenres() {
+  return function (dispatch) {
+    fetch(`http://localhost:3001/genres/`)
+      .then(response => response.json())
+      .then(response => {
+        dispatch({
+          type: GET_GENRES,
+          payload: response,
+        })
+      })
+      .catch(e => console.log(e))
+  }
+}
+
+export function ratingSort(orden){
+
+  return {
+    type : RATING_SORT,
+    payload : orden,
+  }
+}
+// filtro desde la base de datos y api
+export function ordenPorGames (orden){
+  return {
+    type : GAMES_SORT,
+    payload : orden,
+  }
+}
+
+export function filtroPorGenero (genero){
+  return {
+    type : TYPE_GENRO,
+    payload : genero
+  }
+}
+
+export function filtroPorAlpha(orden){
+  return {
+     type : ORDEN_BY_ALPHA,
+     payload : orden
   }
 }
