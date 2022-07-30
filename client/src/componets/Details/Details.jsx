954c746d034a2374211818d2f541647541fdf908
andresyrg16 <andresyrg16@gmail.com>
2022-07-29 21:43:44 -0500

    agregando una imagen por default

--- client/src/componets/Details/Details.jsx
+++ client/src/componets/Details/Details.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { getVideoGamePorId } from '../../redux/actions/action';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams , Link} from 'react-router-dom';
 
 const Details = () => {
+
   const { id } = useParams();
   const detailsVideoGames = useSelector(state => state.videoGameDetails);
   const dispatch = useDispatch();
@@ -12,12 +13,13 @@ const Details = () => {
   useEffect(() => {
     dispatch(getVideoGamePorId(id));
   }, [dispatch]);
-
+  
+  let imgDefault = "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2021/02/30-mejores-heroes-ultimos-30-anos-2243371.jpg?itok=1iWouJJI"
   return (
     <div>
       <h1>details</h1>
       <h3>{detailsVideoGames.name}</h3>
-      <img src={detailsVideoGames.img} alt='' width={300} height={200} />
+      <img src={detailsVideoGames.img? detailsVideoGames.img : imgDefault} alt='' width={300} height={200} />
       <p> Descripción: {detailsVideoGames.description}</p>
       <p>Ranting: {detailsVideoGames.rating}</p>
       <p>Fecha de lanzamiento: {detailsVideoGames.released}</p>
@@ -26,6 +28,8 @@ const Details = () => {
         <ul>
           {detailsVideoGames.genres?.map(p => (
             <li key={p}>{p}</li>
+          )) || detailsVideoGames.Generos?.map(p => (
+            <li key={p.id}>{p.name}</li>
           ))}
         </ul>
       </section>
@@ -37,9 +41,9 @@ const Details = () => {
           ))}
         </ul>
       </section>
-      <button>
-        <a href='/home'>regresar</a>{' '}
-      </button>
+     <Link to="/home">
+      <button>Volver</button>
+     </Link>
     </div>
   );
 };
