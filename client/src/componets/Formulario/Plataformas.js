b222b62189970bca7a85402b836bc2149e75ff7c
andresyrg16 <andresyrg16@gmail.com>
2022-07-29 21:58:04 -0500

    plataformas

---
+++ client/src/componets/Formulario/Plataformas.js
@@ -0,0 +1,22 @@
+
+const platforms = [
+  'PC',
+  'Xbox Series S/X',
+  'PlayStation 4',
+  'PlayStation 5',
+  'Xbox One',
+  'PC',
+  'Nintendo Switch',
+  'Android',
+  'iOS',
+  'macOS',
+  'Linux',
+  'Xbox 360',
+  'PlayStation 3',
+  'Android',
+  'PS Vita',
+  'Web',
+  'PlayStation 2',
+  "Wii U"
+] 
+export default platforms; 
