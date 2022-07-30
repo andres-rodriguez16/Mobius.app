79cde01ed6bc4070867acd7458e8258e00303929
andresyrg16 <andresyrg16@gmail.com>
2022-07-29 21:39:59 -0500

    agregando estilos al app

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
-import './App.css';
+
 import React from 'react';
 import { Route } from "react-router-dom"
 import LandingPage from './componets/landinPage/LandingPage';
 import Home from './componets/Home/Home';
 import NavBar from "./componets/Navbar/Navbar"
 import Details from './componets/Details/Details';
+import Formulario from './componets/Formulario/Formulario';
+import style from "./App.module.css"
 
 function App() {
   return (
-   <React.Fragment>
-    <NavBar />  
-    <Route exact path="/" component={LandingPage} /> 
-    <Route exact path="/home" component={Home} />
-    <Route exact path="/home/:id" component={Details} /> 
-   </React.Fragment>
+    <React.Fragment>
+      <div className={style.background}>
+        <div className={style.transparency}>
+          <NavBar />
+          <Route exact path="/" component={LandingPage} />
+        </div>
+      </div>
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/home/:id" component={Details} />
+      <Route exact path="/createVideogame" component={Formulario} />
+    </React.Fragment>
   );
 }
 
