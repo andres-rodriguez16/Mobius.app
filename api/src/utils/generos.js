0fb465b95341ed30657684826eae4f5703409e9a
andresyrg16 <andresyrg16@gmail.com>
2022-07-31 11:28:33 -0500

    agrengando tryCatch

--- api/src/utils/generos.js
+++ api/src/utils/generos.js
@@ -2,14 +2,21 @@ const axios = require("axios")
 const { YOUR_API_KEY } = process.env;
 const { Genero } = require("../db")
 
-module.exports = {
-  async añadirALaDb() {
+async function añadirALaDb() {
+  try {
     const { data } = await axios.get(`https://api.rawg.io/api/genres?key=${YOUR_API_KEY}`)
     const { results } = data;
      results.map(async (genre) => {
       await Genero.findOrCreate({ where: {name: genre.name} })
     })
     console.log("los generos se guardaron en la DB")
+  } catch (error) {
+    return error;
   }
+  
+}
+
+module.exports = {
+ añadirALaDb
 
 }
