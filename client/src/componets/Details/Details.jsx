534122fab1bb768441b71cc2a2fef594353c7dd1
andresyrg16 <andresyrg16@gmail.com>
2022-09-13 17:15:14 -0500

    cambios

--- client/src/componets/Details/Details.jsx
+++ client/src/componets/Details/Details.jsx
@@ -2,50 +2,56 @@ import React from 'react';
 import { getVideoGamePorId, clearDetails } from '../../redux/actions/action';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams , Link} from 'react-router-dom';
-import s from "./Details.module.css"
+import { useParams, Link } from 'react-router-dom';
+import s from './Details.module.css';
 
 const Details = () => {
-
   const { id } = useParams();
   const detailsVideoGames = useSelector(state => state.videoGameDetails);
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(getVideoGamePorId(id));
     return function () {
-       dispatch(clearDetails())
-    }
+      dispatch(clearDetails());
+    };
   }, [dispatch, id]);
-  
+
   return (
     <div>
-      <Link to="/home">
-      <button className={s.button__volver} >Volver</button>
-     </Link>
-      <h3 className={s.name} >{detailsVideoGames.name}</h3>
-      <img className={s.img} src={detailsVideoGames.img} alt='' />
-      <p className={s.description__p}> Descripción:</p>
-       <p className={s.description__text}>{detailsVideoGames.description}</p>
-      <p className={s.rating}>Ranting: ★ {detailsVideoGames.rating}</p>
-      <p className={s.lanzamiento} >Fecha de lanzamiento: {detailsVideoGames.released}</p>
-      <section className={s.genres}>
-        <p>Generos :</p>
-        <ul>
-          {console.log(detailsVideoGames)}
-          {detailsVideoGames.Generos?.map(p => ( p.name? <li key={p.id}>{p.name}</li>
-            :<li key={p}>{p}</li>
-          ))}
-        </ul>
-      </section>
-      <section  className={s.platforms}>
-        <p>Plataformas : </p>
-        <ul>
-          {detailsVideoGames.platforms?.map(p => (
-            <li key={p}>{p}</li>
-          ))}
-        </ul>
-      </section>
+      <h3 className={s.name}>{detailsVideoGames.name}</h3>
+      <div className={s.container}>
+        <img className={s.img} src={detailsVideoGames.img} alt='' />
+        <p className={s.description__text}>{detailsVideoGames.description}</p> 
+      </div>
+      <div>
+        <div className={s.fecha}>
+        <p className={s.rating}>Ranting: ★ {detailsVideoGames.rating}</p>
+        <br />
+        <p className={s.lanzamiento}>
+          Fecha de lanzamiento: {detailsVideoGames.released}
+        </p>
+        </div>
+        <div>
+        <section className={s.genres}>
+          <p>Generos :</p>
+          <ul>
+            {detailsVideoGames.Generos?.map(p =>
+              p.name ? <li key={p.id}>{p.name}</li> : <li key={p}>{p}</li>
+            )}
+          </ul>
+        </section>
+        <section className={s.platforms}>
+          <p>Plataformas : </p>
+          <ul>
+            {detailsVideoGames.platforms?.map(p => (
+              <li key={p}>{p}</li>
+            ))}
+          </ul>
+        </section>
+        </div>
+      </div>
+      <div>
+      </div>
     </div>
   );
 };
