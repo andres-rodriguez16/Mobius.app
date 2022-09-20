66fd23328bde7111fbbcdffab37f3c253c6a31ad
andresyrg16 <andresyrg16@gmail.com>
2022-09-20 13:53:01 -0500

    volviendo al paginado anterior

--- client/src/componets/Home/Home.jsx
+++ client/src/componets/Home/Home.jsx
@@ -4,61 +4,106 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   getVideoGames,
   getGenres,
+  ordenPorGames,
+  filtroPorGenero,
+  ratingSort,
+  filtroPorAlpha,
   clearHome,
+  ordenFecha
 } from '../../redux/actions/action';
 import Card from '../Card/Card';
 import { Link } from 'react-router-dom';
 import Paginado from '../Paginado/Paginado';
 import Search from '../Search/Search';
 import style from './Home.module.css';
-import loading from '../../img/loading-icon.gif';
-import Filters from '../filters/Filters';
+import loading from "../../img/loading-icon.gif"
+
 const Home = () => {
   const dispatch = useDispatch();
   const videoGames = useSelector(state => state.videoGamesFilter);
+  const genres = useSelector(state => state.genres);
   const [actualPage, setActualPage] = useState(1);
   const [videogamesPorPagina, setvideogamesPorPagina] = useState(15);
   const indeceDelUltimoVideogame = actualPage * videogamesPorPagina;
-  const indeceDelPrimerVideogame =
-    indeceDelUltimoVideogame - videogamesPorPagina;
-  const actualesVideogames = videoGames?.slice(
-    indeceDelPrimerVideogame,
-    indeceDelUltimoVideogame
-  );
+  const indeceDelPrimerVideogame = indeceDelUltimoVideogame - videogamesPorPagina;
+  const actualesVideogames = videoGames?.slice(indeceDelPrimerVideogame,indeceDelUltimoVideogame);
 
   const paginado = numeroDePaginada => {
     setActualPage(numeroDePaginada);
   };
 
-  const nextPag = function () {
-    setActualPage(actualPage + 1)
-    console.log(actualesVideogames);
-}
-const previusPag = function () {
-    setActualPage(actualPage - 1)
-}
   useEffect(() => {
     dispatch(getVideoGames());
     dispatch(getGenres());
-    return function () {
-      dispatch(clearHome());
-    };
+    return function (){
+       dispatch(clearHome())
+    }
   }, [dispatch]);
 
+  function handleOnGames(e) {
+    dispatch(ordenPorGames(e.target.value));
+    setActualPage(1)
+  }
+
+  function handleFiltroPorGenro(e) {
+    dispatch(filtroPorGenero(e.target.value));
+    setActualPage(1);
+  }
+
+  function handleFiltroPorRating(e) {
+    dispatch(ratingSort(e.target.value));
+    setActualPage(1)
+  }
+  function handleFiltroPorAlpha(e) {
+    dispatch(filtroPorAlpha(e.target.value));
+    setActualPage(1)
+  }
+  function handlerFiltroPorFecha(e) {
+    dispatch(ordenFecha(e.target.value))
+  }
   return (
     <div className={style.home}>
       <div className={style.barras}>
-        <Search setActualPage={setActualPage} />
-        <Filters setActualPage={setActualPage} />
+      <Search setActualPage={setActualPage} />
+      <div className={style.filter}>
+        <select onChange={e => handleFiltroPorRating(e)}>
+          <option>Rating</option>
+          <option value='asc'>Ascendente</option>
+          <option value='desc'>Decendente</option>
+        </select>
+        <select onChange={e => handlerFiltroPorFecha(e)}>
+          <option value="fecha">Fecha</option>
+          <option value='asc'>Ascendente</option>
+          <option value='desc'>Decendente</option>
+        </select>
+        <select onChange={e => handleFiltroPorAlpha(e)}>
+          <option value='ordenAlpha'>Alpha</option>
+          <option value='a-z'>A-Z</option>
+          <option value='z-a'>Z-A</option>
+        </select>
+        <select onChange={e => handleOnGames(e)}>
+          <option>Creando en</option>
+          <option value='DB'>Base de datos</option>
+          <option value='Api'>Api</option>
+        </select>
+        <select name='Genros' onChange={e => handleFiltroPorGenro(e)}>
+          <option value='All'>Todos</option>
+          {genres.length
+            ? genres.map(g => {
+                return (
+                  <option value={g.name} key={g.id}>
+                    {g.name}
+                  </option>
+                );
+              })
+            : []}
+        </select>
+      </div>
       </div>
       <Paginado
         videgames={videoGames.length}
         videogamesPorPagina={videogamesPorPagina}
         paginado={paginado}
-        previusPag={previusPag}
-        nextPag={nextPag}
-        actualPage={actualPage}
-        actualesVideogames={actualesVideogames}
       />
       {actualesVideogames.length ? (
         <div className={style.list__cards}>
@@ -67,8 +112,8 @@ const previusPag = function () {
               <Link to={'/home/' + v.id} key={v.id}>
                 <Card
                   name={v.name}
-                  genres={v.Generos}
-                  image={v.img}
+                  genres={v.Generos }
+                  image={v.img }
                   key={v.id}
                   rating={v.rating}
                   released={v.released}
@@ -79,9 +124,10 @@ const previusPag = function () {
         </div>
       ) : (
         <div className={style.loading}>
-          <img src={loading} alt='' />
+          <img src={loading} alt=''  />
         </div>
       )}
+      
     </div>
   );
 };
--- client/src/componets/Paginado/Paginado.jsx
+++ client/src/componets/Paginado/Paginado.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import style from './Paginado.module.css';
-const Paginado = ({
-  videgames,
-  videogamesPorPagina,
-  paginado,
-  nextPag,
-  previusPag,
-  actualPage,
-  actualesVideogames,
-}) => {
+const Paginado = ({ videgames, videogamesPorPagina, paginado }) => {
+  const numerosDePaginas = [];
+  for (let i = 1; i <= Math.ceil(videgames / videogamesPorPagina); i++) {
+    numerosDePaginas.push(i);
+  }
   return (
-    <nav className={style.container}>
-      {actualPage === 1 ? null : (
-        <button onClick={() => previusPag()}>Previus</button>
-      )}
-      <p>{actualPage}</p>
-      {actualesVideogames.length < 15 ? null : (
-        <button onClick={() => nextPag()}>Next</button>
-      )}
+    <nav>
+      <ul className={style.paginado}>
+        {numerosDePaginas &&
+          numerosDePaginas.map(p => (
+            <button className={style.number} key={p} onClick={() => paginado(p)}>
+              {p}
+            </button>
+          ))}
+      </ul>
     </nav>
   );
 };
